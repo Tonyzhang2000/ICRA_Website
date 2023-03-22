68e1b1eec156105e796883e80dccea65987dacef
Tonyzhang2000 <zhangshutong2000@gmail.com>
2023-03-21 21:02:14 -0400

    update web, add buttons and other sections

--- model_click.js
+++ model_click.js
@@ -7,7 +7,11 @@ var modelViewerClick=function(obj, evt){
     // var orbit  = modelViewer.getCameraOrbit();
     // console.log(orbit)
     
-    tie_controls = true
+    var obj_tie = document.getElementById('tie_controls')
+    tie_controls = false
+    if (obj_tie.hasAttribute('tie-interactions')){
+        tie_controls = true
+    }
     if (evt.detail.source == 'user-interaction'){
         // console.log('Hi click on model viewer')
         var orbit = obj.getCameraOrbit();
@@ -42,13 +46,15 @@ var modelViewerClick=function(obj, evt){
         //         text += cars[i] + "<br>";
         //       }
         // })
-        for (let i = 0; i < name_array.length; i++) {
-            if (name_array[i] != text) {
-                obj_array[i].cameraOrbit = orbit.toString()
-                obj_array[i].interactionPrompt = 'none'
-                obj_array[i].jumpCameraToGoal()
+        if (tie_controls){
+            for (let i = 0; i < name_array.length; i++) {
+                if (name_array[i] != text) {
+                    obj_array[i].cameraOrbit = orbit.toString()
+                    obj_array[i].interactionPrompt = 'none'
+                    obj_array[i].jumpCameraToGoal()
+                }
             }
-          }
+        }
         // if (tie_controls){
         //     if (gt_id == text){
         //         // console.log('updating the pred model')
@@ -115,5 +121,55 @@ function modelEventListeners(){
         // })
         el.addEventListener('camera-change', modelViewerClick.bind(event, el), 'false')
     });
+
+}
+
+function enableInteraction() {
+    console.log('Click enableInteraction()')
+    var x = document.getElementById('blink');
+    var start_interaction = true
+    if (x.hasAttribute('no-blink')){
+        x.removeAttribute('no-blink')
+        start_interaction = false
+    }
+    else{
+        var att = document.createAttribute("no-blink");
+        x.setAttributeNode(att);
+        start_interaction = true
+    }
+    if (true){
+        var x = document.getElementsByTagName('model-viewer');
+        // console.log(x)
+        // console.log('button ')
+        // console.log(x.length)
+        Array.from(x).forEach((el) => {
+            // Do stuff here
+            if (start_interaction) {
+                var att = document.createAttribute("camera-controls");
+                el.setAttributeNode(att);
+            }
+            else{
+                el.removeAttribute("camera-controls");
+            }
+
+            
+        });
+    }
+}
+
+function enableInteractionTieControls() {
+    console.log('Click enableInteractionTieControls()')
+    var obj = document.getElementById('tie_controls')
+    
+    if (obj.hasAttribute('tie-interactions')){
+        obj.removeAttribute('tie-interactions')
+        // obj.textContent = "Tie Controls"
+    }
+    else{
+        var att = document.createAttribute("tie-interactions");
+        obj.setAttributeNode(att);
+        console.log('Setting attribute')
+        // obj.textContent = "UnTie Controls"
+    }
 
 }
